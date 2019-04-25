29671c139fe637bbc12c85f7397af87bc6bb8863
Michael Sundt <mikesundt@gmail.com>
2019-04-25 02:01:18 -0400

    added the setRank and compareHands methods

--- Hand.java
+++ Hand.java
@@ -1,7 +1,8 @@
 /**This class will be a set of cards. Going to have a value based on what the hand is, and a method for comparing two hands to one another to see which one is higher */
 public class Hand {
     public static void main(String[] args) {
-        Hand testHand = new Hand("JC TC AC QC KC".split(" "));
+        Hand testHand = new Hand("7C 3C 2H AC KC".split(" "));
+        Hand otherHand = new Hand("2S 2S 7H 4S 6S".split(" "));
         testHand.printHand();
         System.out.println("");
         System.out.println(testHand.toString());
@@ -11,6 +12,8 @@ public class Hand {
         System.out.println("Is this hand a full house? " + testHand.isFullHouse());
         System.out.println("Is this hand a straight? " + testHand.isStraight());
         System.out.println("Is this hand a Royal Flush? " + testHand.isRoyalFlush());
+        System.out.println("Hand Rank: " + testHand.getHandRank());
+        System.out.println("Which hand wins? " + testHand.compareHands(otherHand));
     }
     private Card[] hand = new Card[5];
     private int handRank;
@@ -22,18 +25,65 @@ public class Hand {
         }
         countCards();
         sortHand();
+        setHandRank();
     }
 
-    // private void setHandRank() {
-        //the idea here to to analyze the hand and give it a rank based on what it is. Lower numbers will denote a better hand
-
-    // }
-    private void countCards() {
-        for (int i = 0; i < hand.length; i++) {
-            numberOfCards[hand[i].getIntValue()]++;
+    private void setHandRank() {//This isn't quite finished yet. Need to add an algorithm that makes it so a pair or 4's is a higher rank than a pair of 2's, etc.
+        handRank = 0;
+        if (isRoyalFlush()) {//Royal Flush
+            handRank = 1;
+        } else if (isStraight() && isFlush()) {//Straight Flush
+            handRank = 2;
+        } else if (isFourOfAKind()) {//Four of a Kind
+            handRank = 3;
+        } else if (isFullHouse()) {//Full House
+            handRank = 4;
+        } else if (isFlush()) {//Flush
+            handRank = 5;
+        } else if (isStraight()) {//Straight
+            handRank = 6;
+        } else if (isThreeOfAKind()) {//Three of a Kind
+            handRank = 7;
+        } else if (getNumberOfPairs() == 2) {//Two Pair
+            handRank = 8;
+        } else if (getNumberOfPairs() == 1) {//One Pair
+            handRank = 9;
+        } else {//High Card
+            handRank = 10;
         }
     }
 
+    public int getHandRank() {
+        return handRank;
+    }
+/**Compares two hands to see which one wins the round.
+ * 
+ * @param other the other Hand to compare this hand to
+ * @return 1 if this hand is better than the other hand, or -1 if this hand is worse than the other hand, or 0 if they are the same(which should be impossible)
+ */
+    public int compareHands(Hand other) {
+        int comparison = 0;
+        if (this.getHandRank() < other.getHandRank()) {
+            comparison = 1;
+        } else if (this.getHandRank() > other.getHandRank()) {
+            comparison = -1;
+        } else {
+            for (int i = 0; i < hand.length; i++) {
+                if (this.getCardInHand(i).compareValue(other.getCardInHand(i)) == -1) {
+                    comparison = -1;
+                    break;
+                } else if (this.getCardInHand(i).compareValue(other.getCardInHand(i)) == 1) {
+                    comparison = 1;
+                    break;
+                }
+            }
+        }
+        return comparison;
+    }
+
+    public Card getCardInHand(int index) {
+        return this.hand[index];
+    }
     public boolean isFlush() {
         boolean flush = false;
         if (hand[0].compareSuit(hand[1]) && hand[1].compareSuit(hand[2]) && hand[2].compareSuit(hand[3]) && hand[3].compareSuit(hand[4])) {
@@ -41,6 +91,7 @@ public class Hand {
         }
         return flush;
     }
+
     public int getNumberOfPairs() {
         int numberOfPairs = 0;
         for (int i = 0; i < numberOfCards.length; i++) {
@@ -50,6 +101,7 @@ public class Hand {
         }
         return numberOfPairs;
     }
+
     public boolean isThreeOfAKind() {
         boolean three = false;
         for (int i = 0; i < numberOfCards.length; i++) {
@@ -59,6 +111,7 @@ public class Hand {
         }
         return three;
     }
+
     public boolean isFourOfAKind() {
         boolean four = false;
         for (int i = 0; i < numberOfCards.length; i++) {
@@ -68,6 +121,7 @@ public class Hand {
         }
         return four;
     }
+
     public boolean isFullHouse() {
         boolean fullHouse = false;
         if((this.getNumberOfPairs() != 0) && this.isThreeOfAKind()) {
@@ -75,6 +129,7 @@ public class Hand {
         }
         return fullHouse;
     }
+
     public boolean isStraight() {
         boolean straight = false;
         for (int i = 1; i < hand.length; i++) {
@@ -87,13 +142,14 @@ public class Hand {
         }
         return straight;
     }
-public boolean isRoyalFlush() {
-    boolean royalFlush = false;
-    if(this.isStraight() && this.isFlush() && (hand[0].getValue() == 'A')) {
-        royalFlush = true;
+    
+    public boolean isRoyalFlush() {
+        boolean royalFlush = false;
+        if(this.isStraight() && this.isFlush() && (hand[0].getValue() == 'A')) {
+            royalFlush = true;
+        }
+        return royalFlush;
     }
-    return royalFlush;
-}
     
     public int[] getNumberOfCards() {
         return numberOfCards;
@@ -108,6 +164,7 @@ public boolean isRoyalFlush() {
         }
         return output;
     }
+
     public void printHand() {
         for (int i = 0; i < hand.length; i ++) {
             System.out.print("" + hand[i].getValue() + hand[i].getSuit() + " ");
@@ -130,6 +187,12 @@ public boolean isRoyalFlush() {
                 hand[currentMaxIndex] = hand[i];
                 hand[i] = currentMax;
             }
+        }
+    }
+    //is there a way to use this array to differentiate between two different pairs (or three/four of a kind)?
+    private void countCards() {
+        for (int i = 0; i < hand.length; i++) {
+            numberOfCards[hand[i].getIntValue()]++;
         }
     }
 
